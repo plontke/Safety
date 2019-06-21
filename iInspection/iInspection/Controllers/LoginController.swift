2021a37a79b715ea236d10a878bd3e6985dfaa89
Cohen Plontke <plontke@ualberta.ca>
2019-06-21 16:31:30 -0600

    Initial commit

---
+++ iInspection/iInspection/Controllers/LoginController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginController.swift
+//  iInspection
+//
+//  Created by Cohen Plontke on 2019-06-21.
+//  Copyright © 2019 Cohen Plontke. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ iInspection/iInspection/Controllers/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  iInspection
+//
+//  Created by Cohen Plontke on 2019-06-21.
+//  Copyright © 2019 Cohen Plontke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(handleLogout))
+    
+    }
+
+    @objc func handleLogout() {
+        
+    }
+}
+
